17847a0f36fc65f3c7f0621b7fd15ec001fcda34
JohnDusang <Johndusang1@gmail.com>
2020-10-16 15:46:50 -0600

    updating a second modal on what we do page

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var modal = document.getElementById("simpleModal");
+var modal = document.getElementById("homeModal");
 var modalBtn = document.getElementById("modalBtn");
 var closeBtn = document.getElementsByClassName("closeBtn")[0];
 
@@ -15,6 +15,29 @@ function closeModal() {
 }
 
 function outsideClick(e) {
+    if (e.target == modal) {
+        modal.style.display = "none"
+    }
+};
+
+
+var estateModal = document.getElementById("estateModal");
+var estateModalBtn = document.getElementById("modalBtn1");
+var estateCloseBtn = document.getElementsByClassName("closeBtn1")[0];
+
+modalBtn1.addEventListener("click", openModal1);
+closeBtn1.addEventListener("click", closeModal1);
+window.addEventListener("click", outsideClick1)
+
+function openModal1() {
+    modal.style.display = "block";
+}
+
+function closeModal1() {
+    modal.style.display = "none";
+}
+
+function outsideClick1(e) {
     if (e.target == modal) {
         modal.style.display = "none"
     }
